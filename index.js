f472c137e04ee0946c3c2be878dc609ebc1a7d87
MartinBand <martinbandoni@hotmail.com>
2024-11-11 22:48:53 -0300

    Cambios

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 let formulario = document.getElementById(`formulario`)
 let contenedor2 = document.getElementById(`contenedor2`)
-let contenedor2 = document.getElementById(`contenedor3`)
+let formulario = document.getElementById(`formulario2`)
 
 const ingresarGasto = (event)=>{ 
     event.preventDefault()
